8185b9598e4c9e553d914b64e0c680aa36ea3f3f
Taina <nguyenanhtai24082006@gmail.com>
2026-07-17 09:03:53 +0700

    fix: tách riêng view học viên (_student.blade.php) để loại bỏ hoàn toàn header và footer của student khỏi trang thông báo của giảng viên và admin

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -9,15 +9,5 @@
         </div>
     </x-dynamic-component>
 @else
-    @extends('layouts.app')
-
-    @section('title', 'Thông báo - FEA Learning')
-
-    @section('content')
-    <div class="ui-container py-8 sm:py-12">
-        <div class="mx-auto max-w-4xl space-y-6">
-            @include('notifications._content')
-        </div>
-    </div>
-    @endsection
+    @include('notifications._student')
 @endif
