143419fc1c916e39b54558e3857b79744d9bcddf
Nguyễn Đức Việt <nguyenducv2006@gmail.com>
2026-08-08 02:28:03 +0700

    - Update giao diện form tạo khóa học

--- resources/views/instructor/courses/create.blade.php
+++ resources/views/instructor/courses/create.blade.php
@@ -1,12 +1,13 @@
 <x-instructor-layout title="Tạo khóa học" page-title="Tạo khóa học" breadcrumb="Lưu bản nháp đầu tiên trước khi xây dựng nội dung">
 
-<div class="mx-auto max-w-5xl">
+<div class="w-full min-w-0">
     @include('instructor.courses._form', [
         'course' => null,
         'categories' => $categories,
         'action' => route('instructor.courses.store'),
         'method' => 'POST',
         'submitLabel' => 'Lưu nháp',
+        'wideLayout' => true,
     ])
 </div>
 
