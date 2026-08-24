02b658ea1120c207aec90956522b7ef5310a4d0a
Nguyễn Đức Việt <nguyenducv2006@gmail.com>
2026-08-25 02:37:26 +0700

    Thêm thuộc tính 'standalone' vào layout để hỗ trợ cấu trúc trang độc lập cho giao diện đăng nhập quản trị.

--- resources/views/components/auth/layout.blade.php
+++ resources/views/components/auth/layout.blade.php
@@ -1,3 +1,21 @@
+@props([
+    'standalone' => false,
+    'title' => config('app.name', 'FEA Learning'),
+])
+
+@if ($standalone)
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title }}</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body class="min-h-full bg-white">
+    <x-toast-container />
+@endif
+
 <div class="bg-white">
     <div class="flex min-h-[calc(100vh-16rem)] items-center justify-center px-5 py-10 sm:py-14">
         <div class="mx-auto w-full max-w-[420px]">
@@ -5,3 +23,8 @@
         </div>
     </div>
 </div>
+
+@if ($standalone)
+</body>
+</html>
+@endif
