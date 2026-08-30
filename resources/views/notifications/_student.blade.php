ed927c732449056594cd5fe4fe05802568fcfa88
thaiinguyenn20112006-ux <thaiinguyenn20112006@gmail.com>
2026-08-30 16:16:12 +0700

    xu ly hien thi thong bao hoc vien

--- resources/views/notifications/_student.blade.php
+++ resources/views/notifications/_student.blade.php
@@ -1,4 +1,4 @@
-<x-student-layout title="Thông báo" page-title="Thông báo" breadcrumb="Cập nhật thông tin và hoạt động mới nhất.">
+<x-student-layout title="Thông báo" page-title="Thông báo" breadcrumb="Cập nhật thông báo và hoạt động học tập mới nhất.">
     <div class="space-y-6">
         @include('notifications._content')
     </div>
